9637a474bd1396902ea434e45ed2b5a58cda9678
Emmanuel Santoyo <90350942+EMMANUELSCH19@users.noreply.github.com>
2024-04-23 22:49:00 -0600

    delete project

--- resources/js/Config.jsx
+++
@@ -1,9 +0,0 @@
-import { data } from "autoprefixer";
-import axios from "axios";
-const base_api_url = "http://127.0.0.1:8000/api/v1";
-
-export default {
-  GetRegister: (data) => axios.post(`${base_api_url}/auth/register`, data),
-  GetLogin: (data) => axios.post(`${base_api_url}/auth/login`, data),
-  GetLogout: () => axios.get(`${base_api_url}/auth/logout`),
-}
